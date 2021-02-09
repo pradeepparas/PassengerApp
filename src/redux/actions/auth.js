abc8a27de8285a89101b1888ae92477386334bff
pradeepparas <pradeep.paras@connekt.in>
2021-02-09 19:43:56 +0530

    commit

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import * as myConstClass from '../../screens/constants/constants';
-// import * as actionTypes from './actionTypes';
+import * as actionTypes from './actionTypes';
 // import { ToastContainer, toast } from 'react-toastify';
 import { useHistory } from "react-router-dom";
 // import jwt_decode from "jwt-decode";
@@ -37,41 +37,41 @@ export const auth = (username, password) => {
             value: username,
             password: password,
         };
-      //  let url = 'http://13.126.89.240:8080/user/login';
-        axios.post(api_url+'user/login', authData, {
-                headers: {
-                    'content-type': 'application/json'
-                }
-            })
-            .then(response => {
 
-                if(response.data.data.isActive){
-                    toast.success('Logged In Successfully',{})
-                }
-                localStorage.setItem('token', response.data.token);
-
-                   localStorage.setItem('status', response.data.data.isActive);
-                   localStorage.setItem('orgId', response.data.data.organisation.length>0?response.data.data.organisation[0].organisationId:"");
-
-                   localStorage.setItem('email', response.data.data.email);
-                   localStorage.setItem('username', response.data.data.username);
-                   localStorage.setItem('firstname', response.data.data.name);
-                   localStorage.setItem('lastname', response.data.data.lastName);
-                   localStorage.setItem('image', response.data.data.profileImage);
-
-                   localStorage.setItem('phone', response.data.data.phone);
-
-
-
-                localStorage.setItem('userId', response.data.data._id);
+        // axios.post(api_url+'user/login', authData, {
+        //         headers: {
+        //             'content-type': 'application/json'
+        //         }
+        //     })
+        //     .then(response => {
+        //
+        //         if(response.data.data.isActive){
+        //             toast.success('Logged In Successfully',{})
+        //         }
+        //         localStorage.setItem('token', response.data.token);
+        //
+        //            localStorage.setItem('status', response.data.data.isActive);
+        //            localStorage.setItem('orgId', response.data.data.organisation.length>0?response.data.data.organisation[0].organisationId:"");
+        //
+        //            localStorage.setItem('email', response.data.data.email);
+        //            localStorage.setItem('username', response.data.data.username);
+        //            localStorage.setItem('firstname', response.data.data.name);
+        //            localStorage.setItem('lastname', response.data.data.lastName);
+        //            localStorage.setItem('image', response.data.data.profileImage);
+        //
+        //            localStorage.setItem('phone', response.data.data.phone);
+        //
+        //
+        //
+        //         localStorage.setItem('userId', response.data.data._id);
              dispatch(authSuccess(response.data.token, response.data.data._id));
-                // dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch(err => {
+
+            // })
+            // .catch(err => {
 
                 dispatch(authFail({
                     error: err.response ? err.response.data : null
                 }));
-            });
+            // });
     };
 };
